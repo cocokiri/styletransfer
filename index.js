6f5ab4f250676c2c7021dcb4d4528216ccf5f4f8
Markus <strasser.ms@gmail.com>
2018-03-13 12:47:50 +0100

    image upload to server

--- index.js
+++ index.js
@@ -5,40 +5,53 @@ import Cropper from "cropperjs"
 var image;
 var _URL = window.URL || window.webkitURL;
 
-console.log(Cropper)
-fetch("//192.168.5.165:8080/style.js", {
-    method: 'post',
-    headers: {
-        "Content-type": "application/x-www-form-urlencoded; charset=UTF-8"
-    },
-    body: 'foo=bar&lorem=ipsum'
-})
-    .then(data => console.log(data, "asdasd")
-)
+const file1 = document.getElementById("file")
+
+
+function postImgToServer(image) {
+    const img = new FormData();
+    img.append('type', 'file');
+    img.append('image', image);
+    console.log('IMG ', image)
+    console.log('IMG ', img)
+    console.log('s ', image.src)
+    console.log('d ', img.src)
+
+    fetch("//192.168.5.165:8080/upload", {
+        headers: {
+            'Accept': 'application/json, application/xml, text/plain, text/html, *.*',
+            'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8'
+        },
+        mode:'no-cors',
+        method: 'post',
+        body: img
+    })
+        .catch(err => console.log("ERROR", err))
+        .then(data => console.log('done sending'))
+}
+
+// file1.addEventListener('change', function (e) {
+//     let file;
+//     let src;
+// })
+
+function fileUpload() {
+
+}
 
 $("#file").change(function (e) {
     let file;
     let src;
     if ((file = this.files[0])) {
-        console.log(file)
         image = new Image();
-        image.onload = function () {
-            src = this.src;
-            image.src = _URL.createObjectURL(file);
-            $('#uploadPreview').html('<img src="' + src + '"></div>');
-            e.preventDefault();
-
-            fetch("//192.168.5.165:8080/style.js", {
-                method: 'post',
-                body: new FormData().append('photo', image)
-            })
-                .catch(err => console.log("ERROR", err))
-                .then(data => console.log('done sending'))
-
-
-        }
-        // image.src =
         image.src = _URL.createObjectURL(file);
 
+        image.onload = function () {
+            src = this.src;
+            $('#uploadPreview').html('<img src="' + src + '"></div>');
+
+            postImgToServer(image)
+            e.preventDefault();
+        }
     }
 })
